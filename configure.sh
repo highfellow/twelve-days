50e1fd1d0007d8a5c2cf6af6123aa7fdc8a9f78a
Andy Baxter <highfellow@gmail.com>
2012-08-12 17:56:02 +0100

    using download version of jquery

--- configure.sh
+++ configure.sh
@@ -1,8 +1,6 @@
-mkdir -p lib
+mkdir -p lib/jquery
 cd lib
 git clone git://github.com/fabi1cazenave/webL10n.git
-wget https://github.com/jquery/jquery/tarball/1.8.0
-tar zxvf 1.8.0
-rm 1.8.0
-mv jquery* jquery
-cd ..
+cd jquery
+wget http://code.jquery.com/jquery-1.8.0.min.js
+cd ../..
